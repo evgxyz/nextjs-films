b8b7239c0ea00e48d759edeb4bec11e514d98442
Evgeniy <evgeniy8086@ya.ru>
2023-07-02 15:39:01 +0300

    mysql ok? +dbQuery

--- src/pages/api/film/getInfo.ts
+++ src/pages/api/film/getInfo.ts
@@ -1,16 +1,22 @@
 
 import {NextApiRequest, NextApiResponse} from 'next';
-import {dbQuery} from '@/units/db';
+import {dbQuery, dbEscape} from '@/units/db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const filmId = parseInt(req.query.filmId as string);
+  
+  if (!isFinite(filmId)) {
+    res.status(400).json({error: 'Bad parameters'});
+  }
+  
+  const queryStr = `SELECT * FROM films_table WHERE id=${dbEscape(filmId)}`;
+  const queryResult = await dbQuery(queryStr);
 
-  const queryStr = 'SELECT * FROM `films_table`';
-  const dbQueryResult = await dbQuery(queryStr);
+  const {error, results} = queryResult;
 
-  console.log('results:', results);
-  console.log('fields:', fields);
-
-  res
-  .status(200)
-  .json({});
+  if (!error) {
+    res.status(200).json(results);
+  } else {
+    res.status(500).json({error: 'Fetch data error'});
+  }
 }
--- src/units/db.ts
+++ src/units/db.ts
@@ -1,20 +1,30 @@
 
 import mysql from 'mysql2/promise';
 
+const dbConnConfig = {
+  host: 'localhost',
+  database: 'filmsite',
+  user: 'filmsite',
+  password: 'matreshkin123'
+};
+
 export async function dbQuery(queryStr: string, values?: any) {
   try {
-    const dbConn = await mysql.createConnection({
-      host: 'localhost',
-      database: 'filmsite',
-      user: 'filmsite',
-      password: 'matreshkin123'
-    });
+    const dbConn = await mysql.createConnection(dbConnConfig);
+    const execResult = await dbConn.execute(queryStr, values);
+    const queryResults = Object.assign({}, execResult[0]) as Record<string, unknown>[];
 
-    const result = await dbConn.execute(queryStr, values);
-
-    return result;
+    return {
+      results: queryResults
+    };
   } 
   catch (error) {
-    return {error};
+    return {
+      error: 1
+    };
   }
+}
+
+export function dbEscape(value: any) {
+  return mysql.escape(value)
 }

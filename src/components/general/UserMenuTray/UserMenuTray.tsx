f0af6d6b122621206b87cbb7b585bfcad1e36ab0
Evgeniy <evgeniy8086@ya.ru>
2023-07-10 02:56:32 +0300

    UserMenuTray + Select

--- src/components/general/UserMenuTray/UserMenuTray.tsx
+++ src/components/general/UserMenuTray/UserMenuTray.tsx
@@ -3,6 +3,7 @@ import {useRouter} from 'next/router';
 import {useAppSelector, useAppDispatch} from '@/store';
 import {Lang, isLang, langs, langKeys, langDefault, strlang} from '@/units/lang';
 import {setLang} from '@/store/settings';
+import {Select, SelectCss} from '@/components/common/Select';
 import css from './UserMenuTray.module.scss';
 
 export function UserMenuTray() {
@@ -11,10 +12,7 @@ export function UserMenuTray() {
   const lang = useAppSelector(state => state.settings.lang);
   const dispatch = useAppDispatch();
 
-  const changeLang = function(ev: React.ChangeEvent<HTMLSelectElement>) {
-    ev.preventDefault();
-    
-    const lang = ev.target.value;
+  const changeLang = function(lang: string) {
     if (isLang(lang)) {
       dispatch(setLang(lang as Lang));
       
@@ -32,14 +30,24 @@ export function UserMenuTray() {
 
   return (
     <div className={css['body']}>
-      <select value={lang} onChange={changeLang}>
+      {/* <select value={lang} onChange={changeLang}>
         { langs.map(lang => 
             <option key={lang} value={lang}>
               {strlang(langKeys[lang])}
             </option>
           )
         }
-      </select>
+      </select> */}
+      <Select
+        value={lang} 
+        options={langs.map(lang => ({
+            value: lang, 
+            text: strlang(langKeys[lang])
+          })
+        )}
+        callbackOnSelect={changeLang}
+        css={SelectCss}
+      />
     </div>
   )
 }
